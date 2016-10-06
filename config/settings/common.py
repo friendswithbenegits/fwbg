25533dbc3989c81fe9a65c34284362ea0ab29922
andreffs18 <andreffs18@gmail.com>
2016-10-06 15:44:58 +0100

    Added github social account

--- config/settings/common.py
+++ config/settings/common.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = (
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'allauth.socialaccount.providers.github',  # github
 )
 
 # Apps specific for this project go here.
