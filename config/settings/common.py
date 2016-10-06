e5d1b1c9cff9e117e196735107ad0ef782d484f7
andreffs18 <andreffs18@gmail.com>
2016-10-06 14:16:25 +0100

    Updated configurations to work with local database

--- config/settings/common.py
+++ config/settings/common.py
@@ -14,11 +14,8 @@ import dj_database_url
 
 import os
 import environ
-
-
 ROOT_DIR = environ.Path(__file__) - 3  # (devtinder/config/settings/common.py - 3 = devtinder/)
 APPS_DIR = ROOT_DIR.path('devtinder')
-
 
 
 # APP CONFIGURATION
@@ -103,7 +100,7 @@ MANAGERS = ADMINS
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(os.environ.get('DATABASE_URL'), default='postgres:///devtinder'),
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
