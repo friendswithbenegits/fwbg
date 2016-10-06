4b1fbf0cb7f14255169fe0165c18d84ff51af278
colobas <guilherme.pg.pires@gmail.com>
2016-10-06 18:29:54 +0100

    corrected typos on prev commit

--- crawlers/pixels_github_crawler.py
+++ crawlers/pixels_github_crawler.py
@@ -1,13 +1,15 @@
 from github import *
 import json
+import traceback
 
 def store_person(person, f_people): #nome, avatar, email, empresa, location, repos, n_followers, url
 	p_repos = []
 	for repo in person.get_repos():
-		p_repos.append({
-			"repo_name" : repo.name,
-			"repo_url" : repo.html_url
-		})
+		if repo.owner.name == person.name:
+			p_repos.append({
+				"repo_name" : repo.name,
+				"repo_url" : repo.html_url
+			})
 
 	p_dict = {
 		"name" : person.name,
@@ -19,6 +21,7 @@ def store_person(person, f_people): #nome, avatar, email, empresa, location, rep
 		"repos" : p_repos,
 		"num_followers" : person.followers
 	}
+
 	f_people.write(json.dumps(p_dict)+"\n")
 	print "Stored person: {0}".format(p_dict["name"])
 	return
@@ -36,8 +39,8 @@ def store_repo(repo, f_repos): #nome, n_contribuidores, linguagens, stars, url
 	f_repos.write(json.dumps(r_dict)+"\n")
 	return
 
-with open("login.key") as f:
-	lines = f.readlines()
+with open("login.key", "r") as f:
+	lines = f.read().splitlines()
 
 g = Github(lines[0], lines[1])
 pixels = g.get_organization("PixelsCamp")
@@ -50,17 +53,21 @@ for repo in pixel_repos:
 			try:
 				store_person(person, f_people)
 				for rep in person.get_repos():
-					f_repos = open('repos.json', 'a')
-					try:
-						store_repo(rep, f_repos)
-					except:
-						print "failed to store repo of {0}".format(person.name)
-					finally:
-						f_repos.close()
+					if repo.owner.name == person.name:
+						f_repos = open('repos.json', 'a')
+						try:
+							store_repo(rep, f_repos)
+						except:
+							print "failed to store repo of {0}".format(person.name)
+							traceback.print_exc()
+						finally:
+							f_repos.close()
 
 			except:
 				print "failed to store person and its repos"
+				traceback.print_exc()
 			finally:
 				f_people.close()
 	except:
 		print "failed to store pixels camp repo {0} and its contributors".format(repo)
+		traceback.print_exc()
