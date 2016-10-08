6d93fbeebf4252c849ab8f8ba35c393d33bfbefd
andreffs18 <andreffs18@gmail.com>
2016-10-08 09:23:29 +0100

    Added first_time boolean to user match detail

--- config/views.py
+++ config/views.py
@@ -2,16 +2,27 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.views.generic import TemplateView
+from django.shortcuts import redirect, reverse
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        self.user = self.request.user
+        self.pm = []
+        if self.user.is_authenticated():
+            self.pm = self.user.get_suggestions()
+            # unseen match
+            matches = self.user.get_unseen_matches()
+            # if any matches redirect to other
+            if matches.get('status') != 400:
+                # redirect to match retail
+                match_id = matches.get('matches')[0].id
+                return redirect(reverse("users:match-detail",
+                                        args=(match_id,)))
+        return super(HomeView, self).dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        user = self.request.user
-        if user.is_authenticated():
-            # unseen match
-            context['matches'] = user.get_unseen_matches()
-            # suggestions
-            context['pm'] = user.get_suggestions()
+        context['pm'] = self.pm
         return context
